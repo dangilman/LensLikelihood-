cdd725b7a32c6754f7d13668adbc80de2fa1e3d0
Daniel Gilman <gilmanda@ucla.edu>
2021-12-08 10:54:42 -0400

    upload scripts for Pk analysis

---
+++ lenslikelihood/power_spectrum_util.py
@@ -0,0 +1,7 @@
+def relative_error_systematic(value, relative_error):
+
+    return value * (relative_error + 1)
+
+def absolute_error_systematic(value, absolute_error):
+
+    return value + absolute_error
